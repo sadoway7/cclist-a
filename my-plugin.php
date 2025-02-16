5231eed7181d85b7b36c8550038c273b889f1e1e
sadoway7 <sadoway@gmail.com>
2025-02-15 23:34:38 -0700

    -

--- my-plugin.php
+++ my-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: cclist-admin
  * Description: Product management plugin.
- * Version: 0.1.2.4
+ * Version: 0.1.3.0
  * GitHub Plugin URI: sadoway7/cclist-a
  * GitHub Plugin URI: https://github.com/sadoway7/cclist-a.git
  */
